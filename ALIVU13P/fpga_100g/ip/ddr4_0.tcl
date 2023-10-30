c4ca76c5dded5b09d04a0ff539c6fc71818cb772
masw <masw@masw.tech>
2023-10-30 19:53:30 +0800

    修改 DDR4 TCL 脚本；移除 CMS IP

--- ALIVU13P/fpga_100g/ip/ddr4_0.tcl
+++ ALIVU13P/fpga_100g/ip/ddr4_0.tcl
@@ -2,16 +2,24 @@
 create_ip -name ddr4 -vendor xilinx.com -library ip -module_name ddr4_0
 
 set_property -dict [list \
-    CONFIG.C0.DDR4_AxiSelection {true} \
-    CONFIG.C0.DDR4_AxiDataWidth {512} \
-    CONFIG.C0.DDR4_AxiIDWidth {8} \
-    CONFIG.C0.DDR4_AxiArbitrationScheme {RD_PRI_REG} \
-    CONFIG.C0.DDR4_TimePeriod {833} \
-    CONFIG.C0.DDR4_InputClockPeriod {3332} \
-    CONFIG.C0.DDR4_MemoryType {RDIMMs} \
-    CONFIG.C0.DDR4_MemoryPart {MTA18ASF2G72PZ-2G3} \
-    CONFIG.C0.DDR4_AUTO_AP_COL_A3 {true} \
-    CONFIG.C0.DDR4_CasLatency {17} \
-    CONFIG.C0.DDR4_CasWriteLatency {12} \
-    CONFIG.C0.DDR4_Mem_Add_Map {ROW_COLUMN_BANK_INTLV}
+	CONFIG.C0.DDR4_AxiSelection {true}                   \
+	CONFIG.C0.DDR4_AxiDataWidth {512}                    \
+	CONFIG.C0.DDR4_AxiIDWidth {8}                        \
+	CONFIG.C0.DDR4_AxiArbitrationScheme {RD_PRI_REG}     \
+	CONFIG.C0.DDR4_TimePeriod {833}                      \
+	CONFIG.C0.DDR4_PhyClockRatio	4:1                  \
+	CONFIG.C0.DDR4_InputClockPeriod {2499}               \
+	CONFIG.C0.DDR4_MemoryType	Components               \
+	CONFIG.C0.DDR4_MemoryPart	MT40A512M16HA-083E       \
+	CONFIG.C0.DDR4_Slot	Single                           \
+	CONFIG.C0.DDR4_MemoryVoltage	1.2V                 \
+	CONFIG.C0.DDR4_DataWidth	64                       \
+	CONFIG.C0.DDR4_DataMask	DM_NO_DBI                    \
+	CONFIG.C0.DDR4_Mem_Add_Map	ROW_COLUMN_BANK          \
+	CONFIG.C0.DDR4_Ordering	Normal                       \
+	CONFIG.C0.DDR4_BurstLength	8                        \
+	CONFIG.C0.DDR4_CasLatency	16                       \
+	CONFIG.C0.DDR4_CasWriteLatency	12                   \
 ] [get_ips ddr4_0]
+
+
